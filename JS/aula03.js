20b99c07083ec648f27b7909ff61f39f9549b03d
Felipe_Silvieri <felipesilvieri@yahoo.com>
2023-03-11 08:05:42 -0300

    efefwe

--- JS/aula03.js
+++ JS/aula03.js
@@ -1,11 +1,43 @@
-// 1.5 vetores
+// // funções
 
-const nomes = ['Ana Maria', 'Antonio', 'Berlinda', 'James', 'Stole']
+// const teste = (a, b, c) => {return a * b * c}
+// const hello = () => console.log('Ai papi')
+// hello()
 
-const numeros = [1,2,3,4,5]
-const soma = numeros.reduce((ac, v) => {return ac + v})
-console.log(soma)
+const dobro = valor => valor*2
 
+const ehPar = (n) => {return n % 2 === 0}
+
+console.log(ehPar(5))
+
+  // // Características Arrow Functions;
+// // - Quando uma função receber apenas um parâmetro, não e necessário utilizar os parênteses 
+// a => {console.log(a)}
+
+// const triplo = function(n = 5){
+//   return n * 3
+// }
+// console.log(triplo(7))
+
+
+// function hello(){
+//   console.log('Hello')
+// }
+// hello()
+
+// function hello(nome){
+//   console.log('Hello' + nome)
+// }
+// hello('Flavio')
+
+
+// // 1.5 vetores
+
+// const nomes = ['Ana Maria', 'Antonio', 'Berlinda', 'James', 'Stole']
+
+// const numeros = [1,2,3,4,5]
+// const soma = numeros.reduce((ac, v) => {return ac + v})
+// console.log(soma)
 
 
 // // exercicio : [1,2,3,4,5] => [1,4,9,16,25]
@@ -13,15 +45,16 @@ console.log(soma)
 // const quadrado = numeros.map((numero)=>{return numero**2})
 // console.log(quadrado)
 
+
 // // filter
 // const resultado = nomes.filter((nome) => {return nome.startsWith('A')})
 // console.log(resultado)
 
-// mapeamento
-//['Ana','Pedro'] => ['A','P']
-const letrasIniciais = nomes.map((nome)=>{return nome.charAt(0)})
-console.log(letrasIniciais)
 
+// // mapeamento
+// //['Ana','Pedro'] => ['A','P']
+// const letrasIniciais = nomes.map((nome)=>{return nome.charAt(0)})
+// console.log(letrasIniciais)
 
 
 // // arrow function
@@ -32,15 +65,14 @@ console.log(letrasIniciais)
 // // const resultado = nomes.filter()
 
 
-
 // function soma(a,b){
 //   return (
 //     a + b
 //   );
 // }
 
-// console.log(soma(5,9))
-v = [2, 'abc', true]
+// // console.log(soma(5,9))
+// v = [2, 'abc', true]
 
 
 // v1 = []
